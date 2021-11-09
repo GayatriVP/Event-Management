231293a0a39981096d5aee9e3dccc98fb38062ba
Gayatri Patil <78681507+GayatriVP@users.noreply.github.com>
2021-11-09 20:50:47 +0530

    profile page basic

--- EMS/EMS/urls.py
+++ EMS/EMS/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
+from EMSapp import views
+from django.contrib.auth.models import User
 
 urlpatterns = [
+    path('', include('django.contrib.auth.urls')),
+    url(r'profile/', views.profile, name='profile'),
     path('admin/', admin.site.urls),
 ]
--- EMS/EMSapp/views.py
+++ EMS/EMSapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from EMSapp.models import Customers
 
 # Create your views here.
+
+
+def profile(request):
+    # newrec = Customers(
+    #     cust_id=1,
+    #     name="test account",
+    #     username="test",
+    #     password="test@123",
+    #     email="testemail@gmail.com"
+    # )
+    # newrec.save()
+    uname = "test"
+    myprof = Customers.objects.get(username=uname)
+    res = {"profile": myprof}
+    return render(request, "EMSapp/profile.html", res)
