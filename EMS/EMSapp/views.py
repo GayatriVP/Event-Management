e85fa21359347c677ffb9f17c84567ca850a27c1
Gayatri Patil <78681507+GayatriVP@users.noreply.github.com>
2021-11-30 19:59:45 +0530

    same slot booking error done

--- EMS/EMSapp/views.py
+++ EMS/EMSapp/views.py
@@ -33,21 +33,42 @@ def book_event(request):
         cust_id = Customers.objects.get(cust_id=1)
         event_id = Events.objects.get(event_id=1)
         eve = event_id.location
+        booked_slots = list(Bookings.objects.values_list(
+            'slots', flat=True))
+        booked_slots = booked_slots[0].strip("[]")
+        booked_slots = booked_slots.replace("'", "")
+        booked_slots = booked_slots.replace(" ", "").split(",")
         form = BookEvent(request.POST)
+        booked_date = list(Bookings.objects.values_list(
+            'date', flat=True))
         if form.is_valid():
-            new_booking = Bookings(
-                slots=form.cleaned_data.get('slots'),
-                event_type=form.cleaned_data['event_type'],
-                capacity=form.cleaned_data['capacity'],
-                services=form.cleaned_data.get('services'),
-                location=eve,
-                date=form.cleaned_data['date'],
-                customer_id=cust_id,
-                cust_event_id=event_id
-            )
-            new_booking.save()
-            res = get_prof_det(1)
-            return render(request, "EMSapp/profile.html", res)
+            sel_slots = form.cleaned_data.get('slots')
+            sel_date = form.cleaned_data['date'].strftime('%Y-%m-%d')
+            print(booked_date)
+            print(sel_date)
+            print(type(booked_date))
+            print(type(sel_date))
+
+            output = any(map(lambda each: each in sel_slots, booked_slots))
+            #  and (sel_date in booked_date)
+            if((output == True) and (sel_date in booked_date)):
+                message = "Slot Already selected. Please select another slot or another date"
+                form = BookEvent()
+                return render(request, 'EMSapp/book_event.html', {'form': form, 'error': message})
+            else:
+                new_booking = Bookings(
+                    slots=sel_slots,
+                    event_type=form.cleaned_data['event_type'],
+                    capacity=form.cleaned_data['capacity'],
+                    services=form.cleaned_data.get('services'),
+                    location=eve,
+                    date=sel_date,
+                    customer_id=cust_id,
+                    cust_event_id=event_id
+                )
+                new_booking.save()
+                res = get_prof_det(1)
+                return render(request, "EMSapp/profile.html", res)
 
     else:
         form = BookEvent()
@@ -63,6 +84,8 @@ def delete_event(request, part_id=None):
 
 def get_prof_det(id):
     myprof = Customers.objects.get(cust_id=id)
+    # my = list(booked_slots[0])
+    # print(my)
     bookings = Bookings.objects.filter(
         customer_id=myprof)
     booked = {}
